24498b1bb6f8fcc34424b120104db6a1b84245c2
MrErrol <dominik@suszalski.pl>
2019-12-17 18:28:44 +0100

    test for initialization of a class Nonogram

---
+++ test_nonogram.py
@@ -0,0 +1,20 @@
+import pytest
+import nonogram
+
+def test_Nonogram_initialisation():
+    nono = nonogram.Nonogram("nonograms/small_1.dat")
+    assert nono.nRows == 3
+    assert nono.nCols == 3
+    assert nono.rows == [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+    assert nono.cols == [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+    assert nono.rowHints == [[2], [2], [1, 1]]
+    assert nono.colHints == [[1, 1], [2], [2]]
+    assert nono.rowBlockOrigins == [[0], [0], [0, 0]]
+    assert nono.colBlockOrigins == [[0, 0], [0], [0]]
+    assert nono.rowBlockEndings == [[2], [2], [2, 2]]
+    assert nono.colBlockEndings == [[2, 2], [2], [2]]
+    assert nono.undetermind == 9
+    assert nono.rowsChanged == []
+    assert nono.colsChanged == []
+    assert nono.transposed == False
+    
