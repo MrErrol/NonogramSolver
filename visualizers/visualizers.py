20dc5207c55b20b72c963add33da84b85eafa561
MrErrol <dominik@suszalski.pl>
2020-01-16 15:09:15 +0100

    removed code duplication in plotting function

--- visualizers/visualizers.py
+++ visualizers/visualizers.py
@@ -9,16 +9,18 @@ def update_plot(data, fig, im):
     fig.canvas.draw()
     fig.canvas.flush_events()
 
+def call_imshow(data):
+    return plt.imshow(data, cmap='binary', origin='upper', vmin=-1, vmax=1, \
+                      extent=( 0.5 , 0.5 + len(data[0]), 0.5 , 0.5 + len(data) ) )
+    
 def plot(data, interactive=False):
     if interactive:
         plt.ion()
         fig = plt.figure()
         fig.canvas.draw()
-        im = plt.imshow(data, cmap='binary', origin='upper', vmin=-1, vmax=1, \
-                        extent=( 0.5 , 0.5 + len(data[0]), 0.5 , 0.5 + len(data) ) )
+        im = call_imshow(data)
     else:
         fig = plt.figure()
-        im = plt.imshow(data, cmap='binary', origin='upper', vmin=-1, vmax=1, \
-                        extent=( 0.5 , 0.5 + len(data[0]), 0.5 , 0.5 + len(data) ) )
+        im = call_imshow(data)
         plt.show()
     return fig, im
