7fdef6be6ad0da9e69e3045dcf23bf633bc847c4
MrErrol <dominik@suszalski.pl>
2020-02-17 14:40:22 +0100

    implemented argument parser

--- solver.py
+++ solver.py
@@ -1,29 +1,28 @@
-from lib.nonogram import Nonogram
-from lib.solver import solver
 from time import time
 from sys import argv
+from argparse import ArgumentParser
 
-try:
-    nono = Nonogram(argv[1])
-except:
-    print("Usage: solver.py nonogram_file [interactive]")
-    quit()
+from lib.nonogram import Nonogram
+from lib.solver import solver
+
+def getOptions(args=argv[1:]):
+    parser = ArgumentParser(description="Parses command.")
+    parser.add_argument("-i", "--input", help="Input file with nonogram.")
+    parser.add_argument("-l", "--live", dest='interactive', default=False, action='store_true', help="Live plotting mode.")
+    parser.add_argument("-d", "--depth", dest='searching_depth', type=int, default=2, action='store', help="Searching depth for assumption making.")
+    options = parser.parse_args(args)
+    return options
+
+options = getOptions()
+
+nono = Nonogram(options.input)
+
+if options.interactive:
+    nono.plot(interactive=options.interactive)
     
-try:
-    interactive = argv[2]
-    nono.plot(interactive=interactive)
-except:
-    interactive = False
-    
-try:
-    searching_depth = argv[3]
-except:
-    searching_depth = 2
-
-
 t0 = time()
 
-cycle = solver(nono, searching_depth=searching_depth, interactive=interactive)
+cycle = solver(nono, searching_depth=options.searching_depth, interactive=options.interactive)
 
 if not nono.undetermind:
     print("Solved Nonogram in cycle: " + str(cycle) + ".")
@@ -32,7 +31,7 @@ t1 = time()
 
 print("Solved in : "+str(t1-t0)+'s')
 
-if not interactive:
+if not options.interactive:
     nono.plot()
 else:
     nono.end_iplot()
