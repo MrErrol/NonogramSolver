e4e0367ed3e756d910c682699ddf67de74886ac6
MrErrol <dominik@suszalski.pl>
2020-02-26 16:53:52 +0100

    Corrected test for change in default values

--- tests/test_methods_advanced.py
+++ tests/test_methods_advanced.py
@@ -35,9 +35,9 @@ def test_ivestigate_row_with_assumptions():
     assert nono_assume_2.rows[3] == [-1, -1, 0, 0, 0, -1, -1, -1] # -1 at 5th position is not obvious
 
 def test_search_for_assumptions():
-    assert search_for_assumptions(nono_assume_3) == True
-    assert nono_assume_2.rows[0] == [ 0,  0,  0,  0,  0,  0,  0, -1]
+    assert search_for_assumptions(nono_assume_3, searching_depth=1) == True
+    assert nono_assume_3.rows[0] == [ 0,  0,  0,  0,  0,  0,  0, -1]
     assert nono_assume_3.rows[3] == [-1, -1,  0,  0,  0, -1, -1, -1]
     assert search_for_assumptions(nono_assume_3, searching_depth=2) == True
-    assert nono_assume_2.rows[1] == [ 0,  0,  0,  0,  0,  0,  0, -1]
+    assert nono_assume_3.rows[1] == [ 0,  0,  0,  0,  0,  0,  0, -1]
     assert nono_assume_3.rows[2] == [-1, -1,  0, -1, -1, -1, -1, -1] # cause we already know all about row 3
