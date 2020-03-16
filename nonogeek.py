03bbc135af5d45b5ab619eff82f4b2f452d489b0
MrErrol <dominik@suszalski.pl>
2020-03-16 11:02:19 +0100

    refine nonogeek.py code formatting

--- nonogeek.py
+++ nonogeek.py
@@ -8,12 +8,30 @@ from utils.tools import compare_nonograms
 
 def getOptions(args=argv[1:]):
     parser = ArgumentParser(description="Parses command.")
-    parser.add_argument("input", help="Input file with nonogram.")
-    parser.add_argument("-l", "--live", dest='interactive', default=False, action='store_true', help="Live plotting mode.")
-    parser.add_argument("--hint", dest='hinter', default=False, action='store_true', help="Hinter mode.")
-    parser.add_argument("-v", "--verbose", dest='verbose', type=int, default=1, action='store', help="Sets verbosity for hinter mode. (0-1)")
-    parser.add_argument("-c", "--check", dest='just_check', default=False, action='store_true', help="Verification mode. Checks whether mistake has been made. Does not plot the solution.")
-    parser.add_argument("-d", "--depth", dest='searching_depth', type=int, default=2, action='store', help="Searching depth for assumption making.")
+    parser.add_argument("input",
+                        help="Input file with nonogram.",
+                        )
+    parser.add_argument("-l", "--live",
+                        dest='interactive', default=False, action='store_true',
+                        help="Live plotting mode.",
+                        )
+    parser.add_argument("--hint",
+                        dest='hinter', default=False, action='store_true',
+                        help="Hinter mode.",
+                        )
+    parser.add_argument("-v", "--verbose",
+                        dest='verbose', type=int, default=1, action='store',
+                        help="Sets verbosity for hinter mode. (0-1)",
+                        )
+    parser.add_argument("-c", "--check",
+                        dest='just_check', default=False, action='store_true',
+                        help="Verification mode. Checks whether mistake has \
+                        been made. Does not plot the solution.",
+                        )
+    parser.add_argument("-d", "--depth",
+                        dest='searching_depth', type=int, default=2, action='store',
+                        help="Searching depth for assumption making.",
+                        )
     options = parser.parse_args(args)
     return options
 
@@ -28,11 +46,16 @@ if options.hinter:
     nono.fill_cell = nono.show_hint
     nono.verbose = options.verbose
 
-t0 = time()
 
-cycle = solver(nono, searching_depth=options.searching_depth, interactive=options.interactive)
+timeBeforeSolving = time()
 
-t1 = time()
+cycle = solver(nono,
+               searching_depth=options.searching_depth,
+               interactive=options.interactive,
+               )
+
+timeAfterSolving = time()
+
 
 if options.just_check:
     user_nono = Nonogram(options.input, presolved=True)
@@ -42,7 +65,7 @@ if options.just_check:
 if not nono.undetermind:
     print("Solved Nonogram in cycle: " + str(cycle) + ".")
 
-print("Solved in : "+str(t1-t0)+'s')
+print("Solved in : " + str(timeAfterSolving - timeBeforeSolving) + 's')
 
 if not options.interactive:
     nono.plot()
