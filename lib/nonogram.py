5a8d126cf9118497e210f5335cb1a833f79c0f67
MrErrol <dominik@suszalski.pl>
2020-03-17 12:40:16 +0100

    add dot at the end of sentence printed

--- lib/nonogram.py
+++ lib/nonogram.py
@@ -216,5 +216,5 @@ class Nonogram:
             print("Cell at row=" + str(row+1) + " and col=" + str(col+1) +
                   " may be deduced to be " + values[value])
             if self.hinter == 'assumption_making':
-                print('You will need to analyze more than just single row or column')
+                print('You will need to analyze more than just single row or column.')
         quit()
